f9081cd1f1b83c506eacb3b1ead4d7cc97f7edd6
Red <red.sarouf2014@gmail.com>
2023-07-24 04:58:20 -0400

    add python organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,91 @@
+INSERT INTO bears (  
+  id ,
+  name ,
+  age ,
+  sex ,
+  color ,
+  temperament ,
+  alive 
+  )
+VALUES (
+    1,
+    "Mr. Chocolate",
+    13,
+    "M",
+    "brown",
+    "calm",
+    1
+
+  ),
+ (
+    2,
+    "Rowdy",
+    10,
+    "M",
+    "white",
+    "calm",
+    0
+
+  ),
+ (
+    3,
+    "Tabitha",
+    10,
+    "F",
+    "white",
+    "calm",
+    0
+
+  ),
+ (
+    4,
+    "Sergeant Brown",
+    9,
+    "M",
+    "white",
+    "calm",
+    0
+
+  ),
+
+  (
+    5,
+    "Melissa",
+    21,
+    "F",
+    "dark brown",
+    "calm",
+    0
+
+  ),
+ (
+    6,
+    "Grinch",
+    2,
+    "M",
+    "dark brown",
+    "calm",
+    0
+
+  ),
+ (
+    7,
+    "Wendy",
+    2,
+    "F",
+    "dark yellow",
+    "calm",
+    0
+
+  ),
+   (
+    8,
+    NULL,
+    2,
+    "F",
+    "dark yellow",
+    "calm",
+    0
+
+  );
+
